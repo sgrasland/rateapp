221e5061d6149a3f087d70f56301044de407e400
sgrasland <grasland.sylvain@hotmail.fr>
2021-09-15 16:45:33 +0200

    template version with typescript

---
+++ pages/index.tsx
@@ -0,0 +1,83 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+import { GetServerSideProps  } from 'next'
+
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Salut !</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+
+
+/* Si on veut faire du client-side rendering (classique), on peut faire de la static generation et faire les requêtes dans le composant directement */
+/* Pour faire du data-fetching côté client il existe le hook SWR : */
+/* import useSWR from 'swr'
+
+function Profile() {
+  const { data, error } = useSWR('/api/user', fetch)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+  return <div>hello {data.name}!</div>
+} */
+
+
+/* STATIC GENERATION */
+/* export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+} */
+
+/* SERVER SIDE RENDERING */
+export const getServerSideProps: GetServerSideProps = async context => {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
---
+++ pages/profile.js
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+
+export default function Profile() {
+    return <div>
+            <h2>check profile</h2>
+            <Link href='/'><a>home page</a></Link>
+        </div>
+}
